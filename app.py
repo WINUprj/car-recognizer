df515c9a888e7691a4ea6f2ca6936277aac821a7
winu <harutornof@gmail.com>
2020-07-06 16:26:41 +0900

    added descriptions

--- app.py
+++ app.py
@@ -127,5 +127,6 @@ def predict(img_src):
     return brand
 
 if __name__ == '__main__':
+    # set port at 5000
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
